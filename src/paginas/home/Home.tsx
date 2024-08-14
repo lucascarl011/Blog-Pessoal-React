2340b672d852de35c718702bf75e7253c850e5b1
lucascarl011 <lucassscarlosss54@gmail.com>
2024-08-14 15:40:09 -0300

    Implementando postagens

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,5 +1,9 @@
 import homeLogo from '../../assets/Home.png'
 import './Home.css';
+import ListaPostagens from '../../components/postagens/listaPostagem/ListaPostagem';
+import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
+
+
 
 function Home() {
     return (
@@ -11,9 +15,9 @@ function Home() {
               <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
   
               <div className="flex justify-around gap-4">
-              
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
-              </div>
+              <ModalPostagem />
+              <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+            </div>
             </div>
   
             <div className="flex justify-center ">
@@ -22,7 +26,7 @@ function Home() {
             </div>
           </div>
         </div>
-        
+        <ListaPostagens />
       </>
     );
 }
