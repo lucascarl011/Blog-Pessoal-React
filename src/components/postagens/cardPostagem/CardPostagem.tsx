8d64c46aa523970deb2a1cd9ad8708e7ebcb98a1
lucascarl011 <lucassscarlosss54@gmail.com>
2024-08-18 12:36:11 -0300

    Alterações para deploy

--- src/components/postagens/cardPostagem/CardPostagem.tsx
+++ src/components/postagens/cardPostagem/CardPostagem.tsx
@@ -11,7 +11,7 @@ function CardPostagem({post}: CardPostagemProps) {
     <div className='border-slate-900 border flex flex-col rounded overflow-hidden justify-between'>
       <div>
         <div className="flex w-full bg-indigo-400 py-2 px-4 items-center gap-4">
-          <img src={post.usuario?.foto} className='h-12 rounded-full' alt="" />
+          <img src='https://avatars.githubusercontent.com/u/152232249?v=4' className='h-12 rounded-full' alt="" />
           <h3 className='text-lg font-bold text-center uppercase '>{post.usuario?.nome}</h3>
         </div>
         <div className='p-4 '>
